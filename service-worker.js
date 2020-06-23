78b8c1454398c700fc844424b1ef25da2ab424a9
Adriano Galello <>
2020-06-23 17:03:06 +0200

    Fixed PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-var appShellFiles = [
+var cacheName = 'getadvice-v1';
+var contentToCache = [
   '/',
   '/index.html',
   '/assets/img/android-chrome-192x192.png',
@@ -7,6 +8,8 @@ var appShellFiles = [
   '/assets/img/favicon-16x16.png',
   '/assets/css/main.css',
   '/assets/main.css',
+  'https://kit.fontawesome.com/7dc3015a44.js',
+  'https://unpkg.com/bulma@0.8.0/css/bulma.min.css'
 ];
 
 if ('serviceWorker' in navigator) {
@@ -22,9 +25,25 @@ if ('serviceWorker' in navigator) {
 self.addEventListener('install', function(e) {
   console.log('[Service Worker] Install');
   e.waitUntil(
-    caches.open(appShellFiles).then(function(cache) {
+    caches.open(cacheName).then(function(cache) {
       console.log('[Service Worker] Caching all: app shell and content');
-      return cache.addAll(appShellFiles);
+      return cache.addAll(contentToCache);
+    })
+  );
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: ' + e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
     })
   );
 });
