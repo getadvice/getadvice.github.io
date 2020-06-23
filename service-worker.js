83a2a1bad8d6485874065c472eeb5b234389741e
Adriano Galello <>
2020-06-23 16:45:57 +0200

    Updated pwa

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,14 @@
+var appShellFiles = [
+  '/',
+  '/index.html',
+  '/assets/img/android-chrome-192x192.png',
+  '/assets/img/android-chrome-512x512.png',
+  '/assets/img/favicon-32x32.png',
+  '/assets/img/favicon-16x16.png',
+  '/assets/css/main.css',
+  '/assets/main.css',
+];
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
@@ -6,3 +17,14 @@ if ('serviceWorker' in navigator) {
         });
   });
 }
+
+// Installing Service Worker
+self.addEventListener('install', function(e) {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(appShellFiles).then(function(cache) {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
