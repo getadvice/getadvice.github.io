c438b5df8674e208f0dd46eacc93926d03b156ab
Adriano Galello <>
2020-06-29 20:55:57 +0200

    Updated service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,5 @@
 var cacheName = 'getadvice-v1';
 var contentToCache = [
-  '/',
-  '/index.html',
   '/404.html',
   '/assets/img/android-chrome-192x192.png',
   '/assets/img/android-chrome-512x512.png',
@@ -9,6 +7,7 @@ var contentToCache = [
   '/assets/img/favicon-16x16.png',
   '/assets/css/main.css',
   '/assets/main.css',
+  '/assets/manifest.json',
   'https://kit.fontawesome.com/7dc3015a44.js',
   'https://unpkg.com/bulma@0.8.0/css/bulma.min.css'
 ];
@@ -33,17 +32,15 @@ self.addEventListener('install', function(e) {
   );
 });
 
+
 // Fetching content using Service Worker
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(r) {
       console.log('[Service Worker] Fetching resource: '+e.request.url);
       return r || fetch(e.request).then(function(response) {
-        return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: ' + e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
+        console.log('[Service Worker] Fetching from network: '+e.request.url);
+        return response;
       });
     })
   );
